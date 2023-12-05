c6f9b24ae972d24bf61698013eeb827485e8b3ce
Nathan Salberg <Salbe018@d.umn.edu>
2023-12-04 18:08:52 -0600

    removed polls from http endpoints

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question, Choice, PhoneNumber
+from .models import PhoneNumber
 
-admin.site.register(Question)
-admin.site.register(Choice)
 admin.site.register(PhoneNumber)
